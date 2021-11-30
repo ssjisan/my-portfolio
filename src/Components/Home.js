332013f81e4aa9489db29be09952b30e5d44967e
Sadman Sakib Jisan <sadman.sakibjisan@techhack.ca>
2021-11-30 19:00:44 +0600

    mddown from menu

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import {Grid} from '@mui/material';
-import Menu from './Menu';
+import React from "react";
+import { Grid, Hidden } from "@mui/material";
+import Menu from "./Menu";
 import "./Styles/Home.css";
-import HomeCard from './Components/HomeCard';
-import { Box } from '@mui/system';
+import HomeCard from "./Components/HomeCard";
+import { Box } from "@mui/system";
 
-export default function Home({pages}) {
-    return (
-        <Box className="home-body">
-            <Grid container justifyContent="center" alignContent="center" alignItems="center">
-            <Grid item md={10} style={{padding:"5%"}}>
-                <HomeCard/>
-            </Grid>
-            <Grid item md={2}>
-                <Menu pages={1}/>
-            </Grid>
+export default function Home({ pages }) {
+  return (
+    <Box className="home-body">
+      <Grid
+        container
+        justifyContent="center"
+        alignContent="center"
+        alignItems="center"
+      >
+        <Grid item md={10} style={{ padding: "5%" }}>
+          <HomeCard />
         </Grid>
-        </Box>
-    )
+        <Grid item md={2}>
+          <Hidden mdDown>
+            <Menu pages={1} />
+          </Hidden>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
