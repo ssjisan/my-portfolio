0f47b5e57e258d4a2d9aaeb0b9bfe1f5e1aa7a09
Sadman Sakib Jisan <sadman.sakibjisan@techhack.ca>
2022-01-11 13:10:48 +0600

    experience for mobile added

--- src/Components/Experiences.js
+++ src/Components/Experiences.js
@@ -1,19 +1,38 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Grid, useMediaQuery } from "@mui/material";
 import Menu from "./Menu/Menu";
 import "./Styles/Experience.css";
 import ExperienceCard from "./Components/ExperienceCard";
+import MenuHeaderMobile from "./Menu/MenuHeaderMobile";
+import MenuFooterMobile from "./Menu/MenuFooterMobile";
 export default function Experiences({ pages }) {
+  const fontForMobile = useMediaQuery("(max-width:900px)");
+
   return (
     <div className="experiences-body">
-      <Grid container>
-        <Grid item md={10} style={{padding:"5%"}}>
+      {!fontForMobile && (
+        <Grid container>
+          <Grid item md={10} style={{ padding: "5%" }}>
+            <ExperienceCard />
+          </Grid>
+          <Grid item md={2}>
+            <Menu pages={4} />
+          </Grid>
+        </Grid>
+      )}
+       {fontForMobile && (
+        <Grid container style={{ display: "flex" , justifyContent: "space-between"  }}>
+          <Grid item xs={12} style={{ display:"flex", justifyContent:"center", width:"100%", top:0,maxHeight:"10vh", padding:"2%"}}>
+            <MenuHeaderMobile pages={3}/>
+          </Grid>
+          <Grid item xs={12} style={{padding:"5%",maxHeight:"85vh",overflowY:"scroll"}}>
           <ExperienceCard />
+          </Grid>
+          <Grid item xs={12} style={{display:"flex", justifyContent:"center", width:"100%", position:"fixed", bottom:0,maxHeight:"10vh"}}>
+            <MenuFooterMobile/>
+          </Grid>
         </Grid>
-        <Grid item md={2}>
-          <Menu pages={4} />
-        </Grid>
-      </Grid>
+      )}
     </div>
   );
 }
