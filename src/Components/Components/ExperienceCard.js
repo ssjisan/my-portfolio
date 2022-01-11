5e0757192e02828c7998bda9e770e90fad7cc51c
Sadman Sakib Jisan <sadman.sakibjisan@techhack.ca>
2022-01-11 13:06:50 +0600

    experience added

--- src/Components/Components/ExperienceCard.js
+++ src/Components/Components/ExperienceCard.js
@@ -1,7 +1,6 @@
 import {
   Card,
   CardContent,
-  CardMedia,
   Grid,
   Typography,
   useMediaQuery,
@@ -11,11 +10,8 @@ import React from "react";
 import "../Styles/AboutMe.css";
 import Avatar from "@mui/material/Avatar";
 import job from "../../Assets/experience.json";
-import ReactHtmlParser from "react-html-parser";
 import { useState } from "react";
 import "../Styles/Experience.css";
-import { Link } from "react-router-dom";
-
 export default function ExperienceCard() {
   const forBelow900 = useMediaQuery("(max-width:900px)");
   const forBelow1500 = useMediaQuery("(max-width:1500px)");
@@ -26,25 +22,62 @@ export default function ExperienceCard() {
     <Grid container spacing={1}>
       {experience.map((data) => (
         <Grid item md={4} sm={6} xs={12}>
-          <Card style={{ borderRadius: "6px" }}>
-            <Box className="jobBox">
-              <Box style={{padding:"5%"}}>
-                <Avatar src={
-                data.companyLogo && data.companyLogo.length
-                  ? `${data.companyLogo}`
-                  : `https://ui-avatars.com/api/?color=00756A&name=${data.companyName}`
-              } />
+          <Box
+            style={{
+              padding: "2%",
+              borderRadius: "6px",
+              border: data.end === "present" && "1px solid ",
+            }}
+          >
+            <Card
+              style={{
+                borderRadius: "6px"}}
+            >
+              <Box className="jobBox">
+                <Box
+                  style={{
+                    padding: "5%",
+                  }}
+                >
+                  <Avatar
+                    style={{
+                      width: "100px",
+                      height: "100px",
+                      border: "5px solid #F7F8FA",
+                      backgroundColor: "#fff",
+                      borderRadius: "50%",
+                      margin: "auto",
+                    }}
+                    src={
+                      data.companyLogo && data.companyLogo.length
+                        ? `${data.companyLogo}`
+                        : `https://ui-avatars.com/api/?color=00756A&name=${data.companyName}`
+                    }
+                  />
+                </Box>
               </Box>
-            </Box>
-            <CardContent style={{ padding: "3%" }}>
-              <Typography
-                variant="h5"
-                style={{ fontWeight: "bold", color: "#22215B", height: "50px" }}
-              >
-                {data.companyName}
-              </Typography>
-            </CardContent>
-          </Card>
+              <CardContent style={{ padding: "3%" }}>
+                <Typography
+                  variant="h5"
+                  style={{ fontWeight: "bold", color: "#22215B" }}
+                >
+                  {data.companyName}
+                </Typography>
+                <Typography variant="body1">{data.position}</Typography>
+                <Typography variant="body2">
+                  {data.start}-{data.end}
+                </Typography>
+                <Typography variant="body2" style={{ fontWeight: "bold" }}>
+                  Responsibilities:
+                </Typography>
+                {data.responsibilities.map((data) => (
+                  <Box>
+                    <Typography variant="caption">-{data}</Typography> <br />
+                  </Box>
+                ))}
+              </CardContent>
+            </Card>
+          </Box>
         </Grid>
       ))}
     </Grid>
