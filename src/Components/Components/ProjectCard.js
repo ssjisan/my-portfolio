7fbe184ce71c7d2228e51ef0f7ef25ee134f6316
Sadman Sakib Jisan <sadman.sakibjisan@techhack.ca>
2022-01-11 17:26:33 +0600

    project card added

--- src/Components/Components/ProjectCard.js
+++ src/Components/Components/ProjectCard.js
@@ -1,79 +1,91 @@
-import { Grid, Typography, useMediaQuery } from "@mui/material";
-import { Box } from "@mui/system";
-import React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Collapse,
+  Grid,
+  IconButton,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import React, { useState } from "react";
+import projectData from "../../Assets/projects.json";
 import "../Styles/AboutMe.css";
+import { BsCaretDown } from "react-icons/bs";
+import { AiFillGithub } from "react-icons/ai";
+import { GoGlobe  } from "react-icons/go";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
 export default function ProjectCard() {
   const forBelow900 = useMediaQuery("(max-width:900px)");
   const forBelow1500 = useMediaQuery("(max-width:1500px)");
   const forBelow1200 = useMediaQuery("(max-width:1210px)");
   //   const for955to901 = useMediaQuery("(min-width:901px) and (max-width:960px)");
+  const [project, setProject] = useState(projectData);
+  const [expanded, setExpanded] = useState(false);
+  const handleExpandClick = (id) => {
+    setExpanded(expanded === id ? false : id);
+  };
   return (
-    <Box style={{maxHeight:"79vh", overflowY:"auto"}}>
-      <Grid
-        container
-        spacing={1}
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        alignContent="center"
-        style={{ padding: forBelow900 ? "20px" : "50px" }}
-      >
-        <Grid
-          item
-          xs={12}
-          md={12}
-          style={{ display: "flex", justifyContent: "center" }}
-        >
-          <img
-            src="https://i.ibb.co/g3Xtj8F/react.png"
-            width="50px" height="50px"
-            alt="" 
-          />
-           <img
-            src="https://i.ibb.co/g3Xtj8F/react.png"
-            width="50px" height="50px"
-            alt=""
-          />
-           <img
-            src="https://i.ibb.co/g3Xtj8F/react.png"
-            width="50px" height="50px"
-            alt=""
-          />
+    <Grid container spacing={1}>
+      {project.map((data) => (
+        <Grid item md={4} sm={6} xs={12} >
+          <Card sx={{ maxWidth: 345 }} style={{overflowY:"auto"}}>
+            <CardHeader title={data.title} />
+            <CardMedia
+              component="img"
+              height="194"
+              image={data.image}
+              alt={data.title}
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                This impressive paella is a perfect party dish and a fun meal to
+                cook together with your guests. Add 1 cup of frozen peas along
+                with the mussels, if you like.
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <IconButton aria-label="add to favorites"><GoGlobe/></IconButton>
+              <IconButton aria-label="share">
+                <AiFillGithub />
+              </IconButton>
+              <ExpandMore
+                expand={expanded === data.id}
+                onClick={()=>handleExpandClick(data.id)}
+              >
+                <BsCaretDown />
+              </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded === data.id} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>Tech:</Typography>
+                <Typography paragraph>
+                  Heat 1/2 cup of the broth in a pot until simmering, add
+                  saffron and set aside for 10 minutes.
+                </Typography>
+                <Typography>
+                  Set aside off of the heat to let rest for 10 minutes, and then
+                  serve.
+                </Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Box
-            className="bioScroll"
-            style={{ maxHeight:forBelow900 ? " ": "500px", overflowY:forBelow900 ?"none": "auto" }}
-          >
-            <Typography
-              variant={forBelow900 ? "body1" : "h6"}
-              style={{ textAlign: "justify", padding: "2%" }}
-            >
-              Trying a new thing is fun. New technologies, new ideas. I love to
-              work with versatility. At a very early age at my university, I
-              worked with several things. Firstly I was working with robotics,
-              then with graphics design, and then designing game assets and
-              animation, etc. My final year's project was developing a
-              First-Person Shooting game. I worked as an asset designer in this
-              team. I love learning about new technologies. Now I start learning
-              Networking related staff. Cisco Routing-Switching, Mikrotik
-              Routing. For almost two years, I worked as a network engineer in
-              an Internet service provider(ISP). Recently, I start working with
-              web development,Trying a new thing is fun. New technologies, new
-              ideas. I love to work with versatility. At a very early age at my
-              university, I worked with several things. Firstly I was working
-              with robotics, then with graphics design, and then designing game
-              assets and animation, etc. My final year's project was developing
-              a First-Person Shooting game. I worked as an asset designer in
-              this team. I love learning about new technologies. Now I start
-              learning Networking related staff. Cisco Routing-Switching,
-              Mikrotik Routing. For almost two years, I worked as a network
-              engineer in an Internet service provider(ISP). Recently, I start
-              working with web development,
-            </Typography>
-          </Box>
-        </Grid>
-      </Grid>
-    </Box>
+      ))}
+    </Grid>
   );
 }
