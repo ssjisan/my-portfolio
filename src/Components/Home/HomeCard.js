3e4f98f405e79bb66a4dff299f45ca6fbead79d1
ssjisan <sakibsfet13@gmail.com>
2022-10-27 19:10:57 +0600

    home page

--- src/Components/Home/HomeCard.js
+++ src/Components/Home/HomeCard.js
@@ -2,8 +2,10 @@ import React from "react";
 import { Typography, useMediaQuery, Box } from "@mui/material";
 import TypeAnimation from "./TypeAnimations";
 export default function HomeCard() {
+  const forBelow416 = useMediaQuery("(max-width:416px)");
   const forBelow667 = useMediaQuery("(max-width:667px)");
   const forBelow767 = useMediaQuery("(max-width:767px)");
+  const forBelow900 = useMediaQuery("(max-width:900px)");
   const forBelow999 = useMediaQuery("(max-width:999px)");
 
   return (
@@ -13,25 +15,27 @@ export default function HomeCard() {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        padding: forBelow999 ? "20px" : "50px",
+        paddingTop: forBelow900 && "80px",
       }}
     >
       <Typography
         sx={{
-          fontSize: forBelow667
-            ? "48px"
+          fontSize: forBelow416
+            ? "30px"
+            : forBelow667
+            ? "36px"
             : forBelow767
-            ? "64px"
+            ? "48px"
             : forBelow999
-            ? "76px"
-            : "86px",
+            ? "64px"
+            : "64px",
           fontWeight: 700,
           textAlign: "center",
         }}
       >
         Hello I'm <br /> Sadman Sakib Jisan
       </Typography>
-      <Typography sx={{ fontSize: "20px", color: "#5F656F", fontWeight: 350 }}>
+      <Typography sx={{ fontSize: forBelow667 ? "16px" : "20px", color: "#5F656F", fontWeight: 350 }}>
         <TypeAnimation />
       </Typography>
     </Box>
