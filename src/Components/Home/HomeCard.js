60d4171682ca2791362f419be3acb4473947795e
ssjisan <sakibsfet13@gmail.com>
2022-10-27 15:34:11 +0600

    home page redesign start

--- src/Components/Home/HomeCard.js
+++ src/Components/Home/HomeCard.js
@@ -1,105 +1,53 @@
-import { Grid, Typography, useMediaQuery } from "@mui/material";
-import { Box } from "@mui/system";
-import React, { useEffect } from "react";
-// import { Link } from "react-router-dom";
-import { BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
-import { useDencrypt } from "use-dencrypt-effect";
-const values = [
-  "Front-end development",
-  "Prototyping with figma",
-  "UI/UX designing",
-];
+import React from "react";
+import { Typography, useMediaQuery, Box } from "@mui/material";
+import TypeAnimation from "./TypeAnimations";
 
 export default function HomeCard() {
-  const { result, dencrypt } = useDencrypt();
-  const forBelow900 = useMediaQuery("(max-width:900px)");
-  const forBelow1500= useMediaQuery("(max-width:1500px)");
-  const forBelow1200= useMediaQuery("(max-width:1210px)");
-    useEffect(() => {
-    let i = 0;
-    const action = setInterval(() => {
-      dencrypt(values[i]);
-      if (i === values.length - 1) {
-        i = 0;
-      } else {
-        i = i + 1;
-      }
-    }, 3000);
-    return () => clearInterval(action);
-  }, [dencrypt]);
-  return (
-    <Box>
-      <Grid
-        container
-        spacing={1}
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        alignContent="center"
-        style={{ padding: forBelow900 && "20px" }}
-      >
-        <Grid item xs={12} md={6}>
-          <Typography variant={forBelow900 ? "body1" : "h4"}>
-            Hi, I am
-          </Typography>
-          <Typography variant={(forBelow900 && "h4") ||(forBelow1200 && "h4") || (forBelow1500 && "h3") || "h2"}>
-            Sadman Sakib Jisan
-          </Typography>
-          {!forBelow900 && (
-            <Box style={{ display: "flex"}}>
-              <Typography variant={(forBelow1200 && "body2")||(forBelow1200 && "body1") || "h6"} style={{width:"180px"}}>I Love to work with</Typography>
-              
-              <Typography variant={(forBelow1200 && "body2")||(forBelow1200 && "body1") || "h6"} style={{ color: "red",width:"300px"}}>
-                {result}
-              </Typography>
-            </Box>
-          )}
-          {forBelow900 && (
-            <Box style={{ display: "flex" }}>
-              <Typography variant="body2" style={{ width:"130px"}}>I Love to work with</Typography>
-              
-              <Typography variant="body2" style={{ color: "red", width:"200px"}}>
-                {result}
-              </Typography>
-            </Box>
-          )}
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <img src="https://i.ibb.co/X7Gpy7z/newHome.png" width={forBelow900 ? "280px" : "400px"} alt="" />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Grid container spacing={3}>
-            <Grid item>
-              <a
-                href="https://www.linkedin.com/in/ssjisan/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <BsLinkedin style={{ fontSize: "30px", color: "#0077b5" }} />
-              </a>
-            </Grid>
+  const forBelow667 = useMediaQuery("(max-width:667px)");
+  const forBelow767 = useMediaQuery("(max-width:767px)");
+  const forBelow999 = useMediaQuery("(max-width:999px)");
 
-            <Grid item>
-              <a
-                href="https://github.com/ssjisan"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <BsGithub style={{ fontSize: "30px", color: "#171515" }} />
-              </a>
-            </Grid>
-            <Grid item>
-              <a
-                href="https://www.facebook.com/userjisan/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <BsFacebook style={{ fontSize: "30px", color: "#3b5998" }} />
-              </a>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: forBelow999 ? "20px" : "50px",
+      }}
+    >
+      <Typography
+        sx={{
+          fontSize: forBelow667
+            ? "48px"
+            : forBelow767
+            ? "64px"
+            : forBelow999
+            ? "76px"
+            : "86px",
+          fontWeight: 700,
+        }}
+      >
+        Hello I'm
+      </Typography>
+      <Typography
+        sx={{
+          fontSize: forBelow667
+            ? "48px"
+            : forBelow767
+            ? "64px"
+            : forBelow999
+            ? "76px"
+            : "86px",
+          fontWeight: 700,
+        }}
+      >
+        Sadman Sakib Jisan
+      </Typography>
+      <Typography sx={{ fontSize: "20px", color: "#5F656F", fontWeight: 350 }}>
+        <TypeAnimation />
+      </Typography>
     </Box>
   );
 }
