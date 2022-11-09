d39787de866591b0aa2e174453924ff141da74c6
jisan538 <sadman.sakib@intelli.global>
2022-11-09 19:27:33 +0600

    border added

--- src/Components/Jobs/JobDetailsCard.js
+++ src/Components/Jobs/JobDetailsCard.js
@@ -6,84 +6,109 @@ import { useState } from "react";
 import "../Styles/Experience.css";
 
 export default function JobDetailsCard() {
-    // eslint-disable-next-line
-    const [experience, setExperience] = useState(job);
-    // eslint-disable-next-line
-    const forBelow567 = useMediaQuery("(max-width:567px)");
-    const forBelow999 = useMediaQuery("(max-width:999px)");
-    return (
-        <Box sx={{ display: "flex", flexDirection: "column", width: forBelow999 ? "100%" : "1024px", }}>
-            {experience.map((data, i) => {
-                return (
-                    <Box key={data.id}><Box sx={{ display: "flex", gap: 2, flexDirection: forBelow567 && "column", justifyContent:"flex-start", alignItems:"flex-start"}}>
-                        <Box>
-                            <Avatar
-                                style={{
-                                    width: "60px",
-                                    height: "60px",
-                                    backgroundColor: "#fff",
-                                    margin: "auto",
-                                    objectFit: "cover",
-                                    boxShadow:
-                                        "0px 0px 20px rgba(85, 98, 217, 0.1)",
-                                    borderRadius: "4px",
-                                }}
-                                src={
-                                    data.companyLogo && data.companyLogo.length
-                                        ? `${data.companyLogo}`
-                                        : `https://ui-avatars.com/api/?color=00756A&name=${data.companyName}`
-                                }
-                            />
-                        </Box>
-                        <Box>
-                            <Typography
-                                sx={{
-                                    fontWeight: 700,
-                                    fontSize: " 16px",
-                                }}
-                            >
-                                {data.position}
-                            </Typography>
-                            <Typography
-                                sx={{
-                                    fontWeight: 400,
-                                    fontSize: "14px",
-                                    opacity: "0.6"
-                                }}
-                            >
-                                {data.companyName}
-                            </Typography>
-                            <Typography
-                                sx={{
-                                    fontSize: "14px",
-                                    fontWeight: 400,
-                                    opacity: "0.6"
-                                }}
-                            >
-                                {data.start} - {data.end}
-                            </Typography>
-                            <Typography
-                                sx={{
-                                    fontSize: "14px",
-                                    fontWeight: 400,
-                                    opacity: "0.6"
-                                }}
-                            >
-                                {data.location}
-                            </Typography>
-                            <Typography variant="body2" sx={{ mt: 2 }}>{data.description}
-                            </Typography>
-                            <Typography variant="body2" sx={{
-                                mt: 2, fontWeight: 700,
-                            }}>Responsibilities:</Typography>
-                            {data?.responsibilities.map((data) =>
-                                <Typography variant="body2">• &nbsp; {data}</Typography>)}
-                        </Box>
-                    </Box>
-                        {i !== experience.length - 1 && <Divider sx={{ width: "100%", mt: 4, mb: 4 }} />}
-                    </Box>
-                )
-            })}
-        </Box>
-    );
+  // eslint-disable-next-line
+  const [experience, setExperience] = useState(job);
+  // eslint-disable-next-line
+  const forBelow567 = useMediaQuery("(max-width:567px)");
+  const forBelow999 = useMediaQuery("(max-width:999px)");
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: forBelow999 ? "100%" : "1024px",
+      }}
+    >
+      {experience.map((data, i) => {
+        return (
+          <Box key={data.id}>
+            <Box
+              sx={{
+                display: "flex",
+                gap: 2,
+                flexDirection: forBelow567 && "column",
+                justifyContent: "flex-start",
+                alignItems: "flex-start",
+              }}
+            >
+              <Box>
+                <Avatar
+                  style={{
+                    width: "60px",
+                    height: "60px",
+                    backgroundColor: "#fff",
+                    margin: "auto",
+                    objectFit: "cover",
+                    boxShadow: "0px 0px 20px rgba(85, 98, 217, 0.1)",
+                    borderRadius: "4px",
+                    border: "1px solid rgba(85, 98, 217, 0.4)",
+                  }}
+                  src={
+                    data.companyLogo && data.companyLogo.length
+                      ? `${data.companyLogo}`
+                      : `https://ui-avatars.com/api/?color=00756A&name=${data.companyName}`
+                  }
+                />
+              </Box>
+              <Box>
+                <Typography
+                  sx={{
+                    fontWeight: 700,
+                    fontSize: " 16px",
+                  }}
+                >
+                  {data.position}
+                </Typography>
+                <Typography
+                  sx={{
+                    fontWeight: 400,
+                    fontSize: "14px",
+                    opacity: "0.6",
+                  }}
+                >
+                  {data.companyName}
+                </Typography>
+                <Typography
+                  sx={{
+                    fontSize: "14px",
+                    fontWeight: 400,
+                    opacity: "0.6",
+                  }}
+                >
+                  {data.start} - {data.end}
+                </Typography>
+                <Typography
+                  sx={{
+                    fontSize: "14px",
+                    fontWeight: 400,
+                    opacity: "0.6",
+                  }}
+                >
+                  {data.location}
+                </Typography>
+                <Typography variant="body2" sx={{ mt: 2 }}>
+                  {data.description}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{
+                    mt: 2,
+                    fontWeight: 700,
+                  }}
+                >
+                  Responsibilities:
+                </Typography>
+                {data?.responsibilities.map((data) => (
+                  <Typography variant="body2">• &nbsp; {data}</Typography>
+                ))}
+              </Box>
+            </Box>
+            {i !== experience.length - 1 && (
+              <Divider sx={{ width: "100%", mt: 4, mb: 4 }} />
+            )}
+          </Box>
+        );
+      })}
+    </Box>
+  );
 }
