4176afc68d744a6ac015261927677e2d19ec91e8
ssjisan <sakibfset13@gmail.com>
2023-02-06 11:21:26 +0600

    add reletade blog

--- src/Pages/BlogDetails.js
+++ src/Pages/BlogDetails.js
@@ -1,18 +1,35 @@
-import { Box, Container, Typography, useMediaQuery } from "@mui/material";
+import {
+  Box,
+  Container,
+  Typography,
+  useMediaQuery,
+  Grid,
+  Chip,
+} from "@mui/material";
 import React, { useState, useEffect } from "react";
 import parse from "html-react-parser";
 import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRounded";
 import { Link, useParams } from "react-router-dom";
 import blog from "../Assets/blogData.json";
+const linkStyle = {
+  textDecoration: "none",
+};
 export default function BlogDetails() {
   const forBelow680 = useMediaQuery("(max-width:680px)");
+  const forBelow416 = useMediaQuery("(max-width:416px)");
   const [isScrolling, setIsScrolling] = useState(false);
   const [selectedBlog, setSingleBlog] = useState([]);
+  const [relatedBlog, setRelatedBlog] = useState([]);
   const { path } = useParams();
+ 
   useEffect(() => {
     let selectedBlog = blog.filter((data) => data.link === path);
     setSingleBlog(selectedBlog);
+    let remainBlogs = blog.filter((data) => data.link !== path);
+    remainBlogs.sort(() => Math.random() - 0.5);
+    setRelatedBlog(remainBlogs.slice(0, 4));
   }, [path]);
+  console.log("remain", relatedBlog);
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => {
@@ -153,6 +170,103 @@ export default function BlogDetails() {
               </Box>
             );
           })}
+        </Box>
+        <Box>
+          <Typography>Related Blogs</Typography>
+          <Grid container spacing={forBelow416 ? 1 : 4}>
+            {relatedBlog.map((data) => {
+              return (
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                  <Link to={"/" + data.link} style={linkStyle}>
+                    <Box>
+                      <Box
+                        sx={{
+                          width: "100%",
+                          height: forBelow416 ? "160px" : "180px",
+                          borderRadius: "12px",
+                          overflow: "hidden",
+                        }}
+                      >
+                        <img
+                          src={data.imgUrl}
+                          alt="images"
+                          style={{
+                            width: "100%",
+                            height: forBelow416 ? "160px" : "180px",
+                            objectFit: "cover",
+                          }}
+                        />
+                      </Box>
+                      <Box
+                        sx={{
+                          mt: 1,
+                          display: "flex",
+                          flexDirection: "column",
+                          gap: forBelow416 ? 0.5 : 2,
+                        }}
+                      >
+                        <Box sx={{ display: "flex", gap: 1 }}>
+                          <Chip
+                            label={data.tags}
+                            sx={{
+                              backgroundColor: "#fff1f1",
+                              color: "#ff3535",
+                              fontWeight: 700,
+                            }}
+                          />{" "}
+                        </Box>
+                        <Typography
+                          sx={{
+                            fontSize: forBelow416 ? "14px" : "16px",
+                            fontWeight: 700,
+                            color: "#111827",
+                          }}
+                        >
+                          {data.title}
+                        </Typography>
+                        <Box
+                          sx={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                          }}
+                        >
+                          <Typography
+                            sx={{
+                              display: "flex",
+                              justifyContent: "center",
+                              alignItems: "center",
+                              fontSize: forBelow416 ? "12px" : "14px",
+                              fontWeight: 400,
+                              opacity: 0.6,
+                              color: "#111827",
+                              height: "36px",
+                            }}
+                          >
+                            {data.date}
+                          </Typography>
+                          <Typography
+                            sx={{
+                              display: "flex",
+                              justifyContent: "center",
+                              alignItems: "center",
+                              fontSize: forBelow416 ? "10px" : "14px",
+                              fontWeight: 400,
+                              opacity: 0.6,
+                              color: "#111827",
+                              height: "36px",
+                            }}
+                          >
+                            {data.time}
+                          </Typography>
+                        </Box>
+                      </Box>
+                    </Box>
+                  </Link>
+                </Grid>
+              );
+            })}
+          </Grid>
         </Box>
       </Container>
     </Box>
