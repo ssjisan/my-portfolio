a736b7a6b5274254f39fbe929f3b1dea04ffca92
ssjisan <sakibfset13@gmail.com>
2023-02-15 00:39:36 +0600

    slider start

--- src/Components/ProjectDetails/ImageModal.js
+++ src/Components/ProjectDetails/ImageModal.js
@@ -1,20 +1,49 @@
 import { Box, Modal, Typography } from "@mui/material";
 import React from "react";
+import Slider from "react-slick";
 
 export default function ImageModal({ open, handleClose }) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
     <Modal
       open={open}
       onClose={handleClose}
       sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
     >
-      <Box sx={{ background: "white", width: "75%", height: "90%" }}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Text in a modal
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-        </Typography>
+      <Box
+        sx={{
+          background: "white",
+          width: "75%",
+          height: "90%",
+          outline: "none",
+        }}
+      >
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
       </Box>
     </Modal>
   );
