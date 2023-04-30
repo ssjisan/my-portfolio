7cec0143bdc19d2e867d75bdf3b7fe26109192bc
ssjisan <sakibfset13@gmail.com>
2023-04-30 19:05:37 +0600

    work page redesign

--- src/Components/Jobs/JobDetailsCard.js
+++ src/Components/Jobs/JobDetailsCard.js
@@ -1,4 +1,4 @@
-import { Typography, Box, Avatar, useMediaQuery, Divider } from "@mui/material";
+import { Typography, Box, Grid, useMediaQuery } from "@mui/material";
 import React from "react";
 import job from "../../Assets/experience.json";
 import { useState } from "react";
@@ -8,109 +8,58 @@ export default function JobDetailsCard() {
   const [experience, setExperience] = useState(job);
   // eslint-disable-next-line
   const forBelow416 = useMediaQuery("(max-width:416px)");
+  // eslint-disable-next-line
   const forBelow567 = useMediaQuery("(max-width:567px)");
-  const forBelow999 = useMediaQuery("(max-width:999px)");
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: "column",
-        width: forBelow999 ? "100%" : "1024px",
+        width: "100%",
       }}
     >
       {experience.map((data, i) => {
         return (
-          <Box key={data.id}>
+          <Box sx={{ mb: i !== experience.length - 1 && "128px" }}>
             <Box
               sx={{
-                display: "flex",
-                gap: 2,
-                flexDirection: forBelow567 && "column",
-                justifyContent: "flex-start",
-                alignItems: "flex-start",
+                p: 0.5,
+                backgroundColor: "#EAEAEA",
+                display: "inline-block",
               }}
             >
-              <Box>
-                <Avatar
-                  style={{
-                    width: forBelow416 ? "32px" : "60px",
-                    height: forBelow416 ? "32px" : "60px",
-                    backgroundColor: "#fff",
-                    margin: "auto",
-                    boxShadow: "0px 0px 20px rgba(85, 98, 217, 0.1)",
-                    borderRadius: "4px",
-                    border: "1px solid rgba(85, 98, 217, 0.4)",
-                  }}
-                  src={
-                    data.companyLogo && data.companyLogo.length
-                      ? `${data.companyLogo}`
-                      : `https://ui-avatars.com/api/?color=00756A&name=${data.companyName}`
-                  }
-                />
-              </Box>
-              <Box>
+              {data.end === "present" ? (
                 <Typography
                   sx={{
-                    fontWeight: 700,
-                    fontSize: forBelow416 ? "14px" : "16px",
+                    color: "#555555",
+                    fontSize: "14px",
                   }}
                 >
-                  {data.position}
+                  Currently
                 </Typography>
-                <Typography
-                  sx={{
-                    fontWeight: 400,
-                    fontSize: forBelow416 ? "10px" : "14px",
-                    opacity: "0.6",
-                  }}
-                >
-                  {data.companyName}
-                </Typography>
-                <Typography
-                  sx={{
-                    fontSize: forBelow416 ? "10px" : "14px",
-                    fontWeight: 400,
-                    opacity: "0.6",
-                  }}
-                >
+              ) : (
+                <Typography sx={{ color: "#555555", fontSize: "14px" }}>
                   {data.start} - {data.end}
                 </Typography>
-                <Typography
-                  sx={{
-                    fontSize: forBelow416 ? "10px" : "14px",
-                    fontWeight: 400,
-                    opacity: "0.6",
-                  }}
-                >
-                  {data.location}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  sx={{ mt: 2, fontSize: forBelow416 ? "12px" : "14px" }}
-                >
+              )}
+            </Box>
+            <Grid container spacing={4}>
+              <Grid item sx={12} md={3}>
+                <Box sx={{ display: "flex", flexDirection: "column", gap: 0 }}>
+                  <Typography sx={{ fontSize: "40px", fontWeight: 600 }}>
+                    {data.position}
+                  </Typography>
+                  <Typography sx={{ color: "#555555", fontSize: "16px" }}>
+                    {data.companyName}
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item sx={12} md={6}>
+                <Typography sx={{ color: "#555555", lineHeight: 2 , fontWeight:400}}>
                   {data.description}
                 </Typography>
-                <Typography
-                  variant="body2"
-                  sx={{
-                    mt: 2,
-                    mb: 1,
-                    fontSize: forBelow416 ? "14px" : "16px",
-                    fontWeight: 700,
-                  }}
-                >
-                  Responsibilities:
-                </Typography>
-                {data?.responsibilities.map((data) => (
-                  <Typography sx={{ fontSize: forBelow416 ? "12px" : "14px" }}>
-                    • &nbsp; {data}
-                  </Typography>
-                ))}
-              </Box>
-            </Box>
-            {i !== experience.length - 1 && (
-              <Divider sx={{ width: "100%", mt: 4, mb: 4, borderStyle:"dashed" }} />
-            )}
+              </Grid>
+            </Grid>
           </Box>
         );
       })}
